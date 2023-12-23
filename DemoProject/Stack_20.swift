5fd89afd9f526c094140997124f41a3e727bb39c
LeeeeSuHyeon <tngus0673@naver.com>
2023-12-23 23:57:26 +0900

    20.3 컨테이너의 자식 뷰 제한

--- DemoProject/Stack_20.swift
+++ DemoProject/Stack_20.swift
@@ -19,35 +19,35 @@ import SwiftUI
 
 
 
-struct Stack_20: View {
-    var body: some View {
-        VStack(alignment: .center, spacing: 15){
-            Text("Financial Results")
-                .font(.title)
-            
-            HStack(alignment : .top){
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment : .leading){
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment : .leading){
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                    
-                }
-                .padding(5)
-            }
-            .padding(5)
-        }
-        .padding(5)
-    }
-}
+//struct Stack_20: View {
+//    var body: some View {
+//        VStack(alignment: .center, spacing: 15){
+//            Text("Financial Results")
+//                .font(.title)
+//            
+//            HStack(alignment : .top){
+//                Text("Q1 Sales")
+//                    .font(.headline)
+//                Spacer()
+//                VStack(alignment : .leading){
+//                    Text("January")
+//                    Text("February")
+//                    Text("March")
+//                }
+//                Spacer()
+//                VStack(alignment : .leading){
+//                    Text("$1000")
+//                    Text("$200")
+//                    Text("$3000")
+//                    
+//                }
+//                .padding(5)
+//            }
+//            .padding(5)
+//        }
+//        .padding(5)
+//    }
+//}
 
 
 //struct Stack_20: View {
@@ -60,6 +60,26 @@ struct Stack_20: View {
 //        }
 //    }
 //}
+
+
+
+// 20.3 자식 뷰 제한
+struct Stack_20: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 100){
+            Group{
+                Text("Sample Text")
+                Text("Sample Text")
+                Text("Sample Text")
+                Text("Sample Text")
+                Text("Sample Text")
+                Text("Sample Text")
+            }
+        }
+    }
+}
+
+
 
 #Preview {
     Stack_20()
