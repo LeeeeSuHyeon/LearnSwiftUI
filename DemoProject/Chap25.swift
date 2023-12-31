634ae215c204039187f091cbfffc158a82c8ad26
LeeeeSuHyeon <tngus0673@naver.com>
2023-12-31 12:59:56 +0900

    25. 네비게이션

--- DemoProject/Chap25.swift
+++ DemoProject/Chap25.swift
@@ -64,8 +64,90 @@ import SwiftUI
 //}
 
 
-// 25.3
+//// 25.2
+//struct ToDoItem : Identifiable {
+//    var id = UUID()
+//    var task : String
+//    var imageName : String
+//}
+//
+//
+//struct Chap25: View {
+//    
+//    var listData : [ToDoItem] = [
+//        ToDoItem(task : "Take out the trash", imageName : "trash.circle.fill"),
+//        ToDoItem(task : "Pick up the kids", imageName : "person.2.fill"),
+//        ToDoItem(task : "Wash the car", imageName : "car.fill")
+//    ]
+//    
+//    @State var toggleStatus = true
+//    
+//    var body: some View {
+//        List {
+//            Section(header : Text("Setting")){
+//                Toggle(isOn : $toggleStatus){
+//                    Text("Allow Notification")
+//                }
+//            }
+//           
+//            Section(header : Text("To Do Tasks")){
+//                ForEach(listData){ item in
+//                    HStack{
+//                        Image(systemName: item.imageName)
+//                        Text(item.task)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
+
+// 25.3
+//struct ToDoItem : Identifiable {
+//    var id = UUID()
+//    var task : String
+//    var imageName : String
+//}
+//
+//
+//struct Chap25: View {
+//    
+//    var listData : [ToDoItem] = [
+//        ToDoItem(task : "Take out the trash", imageName : "trash.circle.fill"),
+//        ToDoItem(task : "Pick up the kids", imageName : "person.2.fill"),
+//        ToDoItem(task : "Wash the car", imageName : "car.fill")
+//    ]
+//    
+//    @State var toggleStatus = true
+//    
+//    var body: some View {
+//        NavigationView{
+//            List {
+//                Section(header : Text("Setting")){
+//                    Toggle(isOn : $toggleStatus){
+//                        Text("Allow Notification")
+//                    }
+//                }
+//               
+//                Section(header : Text("To Do Tasks")){
+//                    ForEach(listData){ item in
+//                        HStack{
+//                            NavigationLink(destination: Text(item.task)){
+//                                Image(systemName: item.imageName)
+//                                Text(item.task)
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            .navigationTitle(Text("To Do List"))
+//        }
+//    }
+//}
+
+
+// 25.4
 struct ToDoItem : Identifiable {
     var id = UUID()
     var task : String
@@ -84,24 +166,41 @@ struct Chap25: View {
     @State var toggleStatus = true
     
     var body: some View {
-        List {
-            Section(header : Text("Setting")){
-                Toggle(isOn : $toggleStatus){
-                    Text("Allow Notification")
-                }
-            }
-           
-            Section(header : Text("To Do Tasks")){
-                ForEach(listData){ item in
-                    HStack{
-                        Image(systemName: item.imageName)
-                        Text(item.task)
+        NavigationView{
+            List {
+                Section(header : Text("Setting")){
+                    Toggle(isOn : $toggleStatus){
+                        Text("Allow Notification")
                     }
                 }
+               
+                Section(header : Text("To Do Tasks")){
+                    ForEach(listData){ item in
+                        HStack{
+                            NavigationLink(destination: Text(item.task)){
+                                Image(systemName: item.imageName)
+                                Text(item.task)
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItem)
+                    .onMove(perform : moveItem)
+                }
             }
+            .navigationTitle(Text("To Do List"))
+            .navigationBarItems(trailing: EditButton())
+        }
+        
+        func deleteItem(at offsets: IndexSet){
+            // 데이터 소스에서 항목을 삭제하는 코드
+        }
+        
+        func moveItem(from source : IndexSet, to destination : Int){
+            // 항목을 재배열 하는 코드
         }
     }
 }
+
 
 #Preview {
     Chap25()
