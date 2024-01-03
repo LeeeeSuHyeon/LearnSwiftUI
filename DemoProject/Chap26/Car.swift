d4799fbabc95e6dca0fdee01ddef522a66cd65eb
LeeeeSuHyeon <tngus0673@naver.com>
2024-01-03 16:52:16 +0900

    26. 리스트 구조체 추가

---
+++ DemoProject/Chap26/Car.swift
@@ -0,0 +1,19 @@
+//
+//  Car.swift
+//  DemoProject
+//
+//  Created by 이수현 on 2024/01/03.
+//
+
+import Foundation
+import SwiftUI
+
+struct Car : Codable, Identifiable {
+    var id : String
+    var name : String
+    
+    var description : String
+    var isHybrid : String
+    
+    var imageName : String
+}
---
+++ DemoProject/Chap26/ListNavDemo.swift
@@ -0,0 +1,18 @@
+//
+//  ListNavDemo.swift
+//  DemoProject
+//
+//  Created by 이수현 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ListNavDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListNavDemo()
+}
