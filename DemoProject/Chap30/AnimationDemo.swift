283cb89449b1f183e53bf8572f67b5b1b8eefdaa
LeeeeSuHyeon <tngus0673@naver.com>
2024-01-09 13:37:12 +0900

    30. 애니메이션 반복

--- DemoProject/Chap30/AnimationDemo.swift
+++ DemoProject/Chap30/AnimationDemo.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+//struct AnimationDemo: View {
+//    
+//    @State private var rotation : Double = 0
+//    @State private var scale : CGFloat = 1
+//    
+//    var body: some View {
+//        Button(action: {
+//            self.rotation = (self.rotation < 360 ? self.rotation + 60 : 0)
+//            self.scale = (self.scale < 2.8 ? self.scale + 0.3 : 1)
+//        }){
+//            Text("Click to animate")
+//                .scaleEffect(scale)
+//                .rotationEffect(.degrees(rotation))
+//                .animation(.spring(response: 1, dampingFraction: 0.2, blendDuration: 0))
+//        }
+//    }
+//}
+
+
 struct AnimationDemo: View {
     
     @State private var rotation : Double = 0
@@ -18,10 +37,11 @@ struct AnimationDemo: View {
             self.scale = (self.scale < 2.8 ? self.scale + 0.3 : 1)
         }){
             Text("Click to animate")
-                .rotationEffect(.degrees(rotation))
-                .animation(.spring(response: 1, dampingFraction: 0.2, blendDuration: 0))
                 .scaleEffect(scale)
-        }
+                .rotationEffect(.degrees(rotation))
+//                .animation(Animation.linear(duration: 1).repeatCount(10, autoreverses: false))
+//                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: true))
+       }
     }
 }
 
